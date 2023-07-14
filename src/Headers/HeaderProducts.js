a525f9bb10db8842e2d241ff1b6024fc149fa5b5
Karan Guleria <63366256+karan0-7@users.noreply.github.com>
2023-07-14 06:51:28 +0530

    Add files via upload

---
+++ src/Headers/HeaderProducts.js
@@ -0,0 +1,76 @@
+import React from "react"
+
+
+export default function HeaderProducts()
+{
+  return(<div id="header-products">
+    <div id="header-products-heading"><h1>Cuisines</h1>
+    <img className="arrow" src="https://img.icons8.com/?size=512&id=85436&format=png" alt="not found"/></div>
+    <div id="header-products-list">
+      <div>
+        <img className="icon" src="https://cdn-icons-png.flaticon.com/512/3373/3373278.png" alt="not found" />
+        <div>
+          <h1>Italian Cuisine</h1>
+          <p>Cosa bolle in pentola?</p>
+        </div>
+      </div>
+      <div>
+        <img className="icon" src="https://cdn-icons-png.flaticon.com/512/10600/10600314.png" alt="not found" />
+        <div>
+          <h1>Greek Cuisine</h1>
+          <p>Μου έχεις κάνει τη ζωή πατίνι</p>
+        </div>
+      </div>
+      <div>
+        <img className="icon" src="https://cdn-icons-png.flaticon.com/512/206/206724.png" alt="not found" />
+        <div>
+          <h1>Spanish Cuisine</h1>
+          <p>El Amor Todo lo Puede </p>
+        </div>
+      </div>
+      <div>
+        <img className="icon" src="https://cdn-icons-png.flaticon.com/512/555/555646.png" alt="not found" />
+        <div>
+          <h1>Japanese Cuisine</h1>
+          <p>Itadakimasu</p>
+        </div>
+      </div>
+      <div>
+        <img className="icon" src="https://cdn-icons-png.flaticon.com/512/206/206606.png" alt="not found" />
+        <div>
+          <h1>Indian Cuisine</h1>
+          <p>हमारे मसाले चखने लायक हैं</p>
+        </div>
+      </div>
+      <div>
+        <img className="icon" src="https://cdn-icons-png.flaticon.com/512/206/206600.png" alt="not found" />
+        <div>
+          <h1>Mexican Cuisine</h1>
+          <p>Al mal tiempo, buena cara</p>
+        </div>
+      </div>
+      <div>
+        <img className="icon" src="https://cdn-icons-png.flaticon.com/512/555/555560.png" alt="not found" />
+        <div>
+          <h1>Turkish Cuisine</h1>
+          <p>Biz ayrı dünyaların insanlarıyız.</p>
+        </div>
+      </div>
+      <div>
+        <img className="icon" src="https://cdn-icons-png.flaticon.com/512/206/206626.png" alt="not found" />
+        <div>
+          <h1>American Cuisine</h1>
+          <p>The best of burgers</p>
+        </div>
+      </div>
+      <div>
+        <img className="icon" src="https://cdn-icons-png.flaticon.com/512/330/330490.png" alt="not found" />
+        <div>
+          <h1>French Cuisine</h1>
+          <p>La bonne nourriture fait la belle figure</p>
+        </div>
+      </div>
+
+    </div>
+  </div>)
+}
